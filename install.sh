25c547c51799dd8fcdd5e21958007b5056db69aa
w3irdguy <bashlover142@gmail.com>
2024-08-22 06:30:48 -0300

    h3ll0 fr13nd

---
+++ install.sh
@@ -0,0 +1,4 @@
+sudo apt install aircrack-ng -y
+chmod +x wpacracker
+sudo mv wpacracker /bin
+echo "to use wpacracker, go to an directory with 2 handshakes or more(EX: HS_S23HD445K5O6J.pcap) and type 'wpacracker'"
